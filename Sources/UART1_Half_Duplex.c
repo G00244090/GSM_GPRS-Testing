cab7285a9f2c8047ff16d8b17eafc49999baf5f1
G00244090 <g00244090@gmit.ie>
2016-01-25 22:55:41 +0000

    Ability to send a sms message is now possible
    A fixed string message can be sent.Not all vital at commands included
    just a basic set.
    Note: response byte size needs to be known and entered for response
    buffer to correctly work.

--- Sources/UART1_Half_Duplex.c
+++ Sources/UART1_Half_Duplex.c
@@ -16,6 +16,7 @@ int result = 5;
 #define SUCCESS 0
 #define FAIL -1
 #define ERROR -2
+uart_status_t returnValue;
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
@@ -29,59 +30,46 @@ void UART1_IRQHandler(void){
 	UART_DRV_IRQHandler(1);
 }
 
-int send_command(int instance, char * command_ptr, int byteCountBuff, int wait_time,char * response ){
+int send_command(int instance, char * command_ptr, int byteCountBuff, int wait_time,char * response,int recieve_size){
 	PRINTF("\r\nInside send command\r\n");
 	int i =0, x;
+	for(x=0;x<200;x++)
+		response[x]=0;
 	UART_DRV_SendDataBlocking(instance, command_ptr, byteCountBuff, wait_time);
 	printf("Sent\r\n");
-	 while(UART_DRV_ReceiveDataBlocking (instance, response, sizeof(response),200000u) == kStatus_UART_Success ){
-		 PRINTF("Recieved:\r\n");
-		 for(i=0;i<=sizeof(response);i++)
-		 {
-//			 if(response[i] == '\r')
-//				 PRINTF("HI\n");
-//		 while(response[i] != '\0')
-//		 {
+	printf("Size of response1 %d\r\n",sizeof(response));
+	returnValue = UART_DRV_ReceiveDataBlocking (instance,response,recieve_size,wait_time);//Buffer size in bytes to accept all of the me
+	printf("Size of response2 %d\r\n",sizeof(response));
+	if(returnValue == kStatus_UART_Success){
+		PRINTF("Fn Recieved:\r\n");
+		while(response[i] != '\0')
+		{
 			 PRINTF("%c",response[i]);
-//			 if(response[i] == '\r')
-//						 PRINTF("\\r");
-//			 if(response[i] == '\n')
-//									 PRINTF("\\n");
-			 //i++;
-		 }
+			 i++;
+		}
+		PRINTF("\r\n");
 
-		 //PRINTF("%c",response[i]);
-		 //}
-//		 while(*response != '\0')
-//		 	{
-//		 		PUTCHAR(*response);
-//		 		if(*response++ == '\r')
-//		 			PUTCHAR('\n');
-//		 	}
-	 }
-//	 if(strcmp(response[0],"ER") ){
-//		PRINTF("\nRecived OK in UART\n\r");
-//		return 0;
-//	}
-//	 else
-//	 {
-//		 return -2;
-//	 }
+		if(strstr(*response,"OK") || strstr(*response,">") || strstr(*response,"+CMSG:")  == 0){
+			printf("Response matches\r\n");
+			return 0;
+		}
 
+		 //return -2;
+	}
 }
 int main(void)
 {
 	uart_state_t uartState; // user provides memory for the driver state structure
 	uart_user_config_t uartConfig;
 	long int x;
+	int recieve_size=0;
+	char AT[] = "AT\r";
+	char CMGF[] = "AT+CMGF=1\r";
+	char CMGS[] = "AT+CMGS=\"+353877763894\"\r";
+	char MESSAGE[] = "HELLO AONGHUS KL26Z \x1A";
 
-
-	char AT[] = "AT+GMI\r";
-	char PIN_CHECK[] = "AT+CPIN?\r";//Setting up a char variable PIN_CHECK for the a long string
-	//char  PIN_ENTER[] = "AT+CPIN=\"1234\"\r\n";//Setting up a char variable ENTER_PIN for the a long string
-	//char  CREG[] = "AT+CREG?\r\n";//Setting up a char variable CREG for the a long string
-	uint8_t response[100];
-	enum STATES {INIT, CHECK_PIN,ENTER_PIN};
+	char response[200];
+	enum STATES {INIT,TXT_MODE,SEND_SMS,SEND_MSG};
 	enum STATES CurrentState = INIT;
 	uint32_t byteCountBuff = 0;
     //Initialise the FRDM-KL26Z Board
@@ -97,47 +85,54 @@ int main(void)
 	uartConfig.stopBitCount = kUartOneStopBit;
 
 	UART_DRV_Init(1, &uartState,&uartConfig);
-
 	PRINTF("Full test of send/recieve on UART1\r\n");
-
 	int i = 0,y;
-	for(y=0;y<sizeof(response);y++)
-		response[i]=0;
+	for(y=0;y<=sizeof(response);y++)
+		response[i]='c';
 
 	byteCountBuff = sizeof(AT);
 	wait_time = 16000u;
-    while(1) {
-    	switch(CurrentState)
-    	 		{
-    	 		case INIT:	//Check connection to MODEM by sending AT. Expected response is OK
-    	 			result = send_command(1,AT,byteCountBuff,wait_time,response);
-    	 			if(result == SUCCESS)	//"OK" was returned by MODEM
-    	 				CurrentState = CHECK_PIN;
-    	 			break;
-    	 		case CHECK_PIN:	//Check connection to MODEM by sending AT. Expected response is OK
-					PRINTF("CHECK_PIN\r");
-//					byteCountBuff = sizeof(PIN_CHECK);
-//					result = send_command(1,PIN_CHECK,byteCountBuff,wait_time,response);
-//					if(result == SUCCESS)	//"OK" was returned by MODEM
-//						CurrentState = ENTER_PIN;
-//					else if(result == ERROR)
-//						CurrentState = ENTER_PIN;
-					break;
-    	 		case ENTER_PIN:	//Check connection to MODEM by sending AT. Expected response is OK
-					PRINTF("ENTER_PIN\r");
-//					result = send_command(1,AT,byteCountBuff,wait_time,response);
-//					//send_command(int instance, char * command_ptr, int byteCountBuff, int wait_time)
-//					//send_command(AT, response, sizeof(response), 2000);
-//					if(result == SUCCESS || result == ERROR)
-//					{
-//						//printf_response(response);
-//					}
-//					if(result == SUCCESS)	//"OK" was returned by MODEM
-//						CurrentState = CHECK_PIN;
-					break;
-    	 		default:
-    	 			break;
-    	 		}//end switch-case
+    while(1){
+    	switch(CurrentState){
+			case INIT:	//Check connection to MODEM by sending AT. Expected response is OK
+				recieve_size =4;
+				byteCountBuff = sizeof(AT);
+				result = send_command(1,AT,byteCountBuff,wait_time,response,recieve_size);
+				if(result == SUCCESS){
+					printf("returned INIT\r\n");//"OK" was returned by MODEM
+					CurrentState = TXT_MODE;
+				}
+				break;
+			case TXT_MODE:	//Check connection to MODEM by sending AT. Expected response is OK
+				recieve_size =4;
+				byteCountBuff = sizeof(CMGF);
+				result = send_command(1,CMGF,byteCountBuff,wait_time,response,recieve_size);
+				if(result == SUCCESS){
+					printf("returned TXT_MODE\r\n");//"OK" was returned by MODEM
+					CurrentState = SEND_SMS;
+				}
+				break;
+			case SEND_SMS:	//Check connection to MODEM by sending AT. Expected response is OK
+				recieve_size = 2;
+				byteCountBuff = sizeof(CMGS);
+				result = send_command(1,CMGS,byteCountBuff,wait_time,response,recieve_size);
+				if(result == SUCCESS){
+					printf("returned SEND_SMS\r\n");//"OK" was returned by MODEM
+					CurrentState = SEND_MSG;
+				}
+				break;
+			case SEND_MSG:	//Check connection to MODEM by sending AT. Expected response is OK
+				recieve_size =8;
+				byteCountBuff = sizeof(MESSAGE);
+				result = send_command(1,MESSAGE,byteCountBuff,wait_time,response,recieve_size);
+				if(result == SUCCESS){
+					printf("returned SEND_MSG\r\n");//"OK" was returned by MODEM
+					//CurrentState = CHECK_PIN;
+				}
+				break;
+			default:
+				break;
+		}//end switch-case
     }
     /* Never leave main */
     return 0;
